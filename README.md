805bc742c4933ec734b7474f3ddda134d96bf9ef
AlbertronicDev <development@albertronic.com>
2022-08-29 17:29:34 +0200

    First stable version

---
+++ README.md
@@ -0,0 +1,29 @@
+# Dutch SmartMeter MQTT to DBus
+**This project currently only works for Dutch smart meters**
+This software takes the Dutch smart meter values on the VenusOS MQTT and translates it to the correct D-Bus values.
+It involves the Smartstuff P1 Pro Dongle, which sends the Dutch smart meter values from the P1 port to the VenusOS local MQTT broker.
+
+## Dutch Smart Meter P1 module
+This project uses the [Smartstuff P1 Pro Dongle](https://smart-stuff.nl/product/p1-dongel-slimme-meter-esp32/), which contains the ESP32C3.
+This module can be configured to send the smart meter data to a MQTT broker. In this case: the Victron GX local MQTT broker.
+
+## Smartstuff P1 Pro Dongle configuration
+Sending the smart meter P1 data to the MQTT is quite simple:
+1. Get the Smartstuff P1 Pro Dongle **with DSMR-API preinstalled**.
+2. Connect via WiFi to this module and place it on the same network as the Victron GX. When on the same network, open the [P1 Dongle Pro configuration page](http://p1-dongle-pro/)
+3. Go to Settings. Enter the Victron GX IP at MQTT Broker IP. Set MQTT Top Topic to "SmartMeter/"
+4. Enable the MQTT broker on the Victron GX: go to Settings->Services and enable "MQTT on LAN (SSL)". Also enable "MQTT on LAN (Plaintext)"
+
+## Software intallation
+To use this software, you need to install kwindrem's [PackageManager](https://github.com/kwindrem/SetupHelper).
+If you use the 123\SmartBMS to USB on your Victron GX, then PackageManager was already installed together with the [123\SmartBMS VenusOS](https://123electric.eu/products/123smartbms-to-usb/) software.
+
+From PackageManager, add a new package.
+Github username: 123electric
+Repository: SmartMeter-DBus
+Branch: latest
+
+After activation of this software, the software will get the MQTT values from the Victron GX broker and send it to the DBus.
+If you want to use this meter for ESS, go to Settings->ESS->Grid metering and select "External meter".
+
+The Victron remote should now show your smart meter values and if activated, use the values for ESS grid compensation.
---
+++ SmartMeterDBus.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python3
+import os
+from os import _exit as os_exit
+import sys
+import time
+import ssl
+from paho.mqtt.client import Client
+from collections import deque
+from datetime import datetime
+from traceback import print_exc
+from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import GLib
+
+# Victron packages
+sys.path.insert(1, os.path.join(os.path.dirname(__file__), '/opt/victronenergy/dbus-systemcalc-py/ext/velib_python'))
+import dbus.service
+import ve_utils
+from vedbus import VeDbusService
+
+class SmartMeterDBus():
+    def __init__(self, mqtt_broker, mqtt_username, mqtt_password, mqtt_topic):
+        self._info = {
+            'name'      : "SmartMeter",
+            'servicename' : "smartmeter",
+            'id'          : 0,
+            'version'    : 1.0
+        }
+        self._device_instance = 30
+
+        self.last_updated = None
+        self._mqtt_topic = mqtt_topic
+        self._mqtt_client = Client('SmartMeterDBus')
+        self._mqtt_client.tls_set(cert_reqs=ssl.CERT_NONE)
+        self._mqtt_client.tls_insecure_set(True)
+        self._mqtt_client.username_pw_set(mqtt_username, password=mqtt_password)
+        self._mqtt_client.connect(mqtt_broker, port=8883)
+        self._mqtt_client.on_connect = self._mqtt_on_connect
+        self._mqtt_client.on_message = self._mqtt_on_message
+        self._mqtt_client.loop_start()
+        self._meter_data = {}
+
+        self._dbusservice = VeDbusService("com.victronenergy.grid.smartmeter")
+        
+        # Create the management objects, as specified in the ccgx dbus-api document
+        self._dbusservice.add_path('/Mgmt/ProcessName', __file__)
+        self._dbusservice.add_path('/Mgmt/ProcessVersion', self._info['version'])
+        self._dbusservice.add_path('/Mgmt/Connection', '(API)')
+
+        # Create the basic objects
+        self._dbusservice.add_path('/DeviceInstance', self._device_instance)
+        self._dbusservice.add_path('/ProductId',     self._info['id'])
+        self._dbusservice.add_path('/ProductName',     self._info['name'])
+        self._dbusservice.add_path('/FirmwareVersion', self._info['version'], gettextcallback=lambda p, v: "v{:.2f}".format(v))
+        self._dbusservice.add_path('/HardwareVersion', None)
+        self._dbusservice.add_path('/Serial', None)
+        self._dbusservice.add_path('/Connected', 1)
+
+        # Create device list
+        self._dbusservice.add_path('/Devices/0/DeviceInstance',  self._device_instance)
+        self._dbusservice.add_path('/Devices/0/FirmwareVersion', self._info['version'])
+        self._dbusservice.add_path('/Devices/0/ProductId',       self._info['id'])
+        self._dbusservice.add_path('/Devices/0/ProductName',   self._info['name'])
+        self._dbusservice.add_path('/Devices/0/ServiceName',   self._info['servicename'])
+        self._dbusservice.add_path('/Devices/0/VregLink',     "(API)")
+
+        # Create the meter paths
+        self._dbusservice.add_path('/Ac/Power', None, gettextcallback=lambda p, v: "{:d}W".format(v))
+        self._dbusservice.add_path('/Ac/Current', None, gettextcallback=lambda p, v: "{:.2f}A".format(v))
+        self._dbusservice.add_path('/Ac/Energy/Forward', None, gettextcallback=lambda p, v: "{:.1f}kWh".format(v))
+        self._dbusservice.add_path('/Ac/L1/Power', None, gettextcallback=lambda p, v: "{:d}W".format(v))
+        self._dbusservice.add_path('/Ac/L2/Power', None, gettextcallback=lambda p, v: "{:d}W".format(v))
+        self._dbusservice.add_path('/Ac/L3/Power', None, gettextcallback=lambda p, v: "{:d}W".format(v))
+        self._dbusservice.add_path('/Ac/L1/Voltage', None, gettextcallback=lambda p, v: "{:.1f}V".format(v))
+        self._dbusservice.add_path('/Ac/L2/Voltage', None, gettextcallback=lambda p, v: "{:.1f}V".format(v))
+        self._dbusservice.add_path('/Ac/L3/Voltage', None, gettextcallback=lambda p, v: "{:.1f}V".format(v))
+        self._dbusservice.add_path('/Ac/L1/Current', None, gettextcallback=lambda p, v: "{:.2f}A".format(v))
+        self._dbusservice.add_path('/Ac/L2/Current', None, gettextcallback=lambda p, v: "{:.2f}A".format(v))
+        self._dbusservice.add_path('/Ac/L3/Current', None, gettextcallback=lambda p, v: "{:.2f}A".format(v))
+
+    def update(self):
+        #if topic == 'power_delivered_l1' and round(float(value)*1000, 3) > 0: self._dbusservice["/Ac/L1/Power"] = float(message.payload)*1000
+        power_l1 = int(self._meter_data['power_returned_l1']*-1000) if round(self._meter_data['power_returned_l1'], 3) > 0.000 else int(self._meter_data['power_delivered_l1']*1000)
+        power_l2 = int(self._meter_data['power_returned_l2']*-1000) if round(self._meter_data['power_returned_l2'], 3) > 0.000 else int(self._meter_data['power_delivered_l2']*1000)
+        power_l3 = int(self._meter_data['power_returned_l3']*-1000) if round(self._meter_data['power_returned_l3'], 3) > 0.000 else int(self._meter_data['power_delivered_l3']*1000)
+        voltage_l1 = self._meter_data['voltage_l1']
+        voltage_l2 = self._meter_data['voltage_l2']
+        voltage_l3 = self._meter_data['voltage_l3']
+        current_l1 = round(power_l1/voltage_l1, 2)
+        current_l2 = round(power_l2/voltage_l2, 2)
+        current_l3 = round(power_l2/voltage_l3, 2)
+        self._dbusservice['/Ac/Power'] = power_l1+power_l2+power_l3
+        self._dbusservice['/Ac/Current'] = current_l1+current_l2+current_l3
+        self._dbusservice['/Ac/Energy/Forward'] = round(self._meter_data['energy_delivered_tariff1']+self._meter_data['energy_delivered_tariff2'], 2)
+        self._dbusservice['/Ac/L1/Power'] = power_l1
+        self._dbusservice['/Ac/L2/Power'] = power_l2
+        self._dbusservice['/Ac/L3/Power'] = power_l3
+        self._dbusservice['/Ac/L1/Voltage'] = self._meter_data['voltage_l1']
+        self._dbusservice['/Ac/L2/Voltage'] = self._meter_data['voltage_l2']
+        self._dbusservice['/Ac/L3/Voltage'] = self._meter_data['voltage_l3']
+        self._dbusservice['/Ac/L1/Current'] = current_l1
+        self._dbusservice['/Ac/L2/Current'] = current_l2
+        self._dbusservice['/Ac/L3/Current'] = current_l3
+
+    def close(self):
+        self._mqtt_client.loop_stop()
+
+    def _mqtt_on_message(self, client, userdata, message):
+        self.last_updated = time.time()
+        # Get sub topic from whole topic path
+        topic = message.topic[len(self._mqtt_topic)+1:]
+        try:
+            # If value contains point, then try to convert to float. Otherwise to integer. If conversions fails, convert to string
+            if '.' in str(message.payload):
+                val = float(message.payload)
+            else:
+                val = int(message.payload)
+        except ValueError:
+            val = str(message.payload)
+        self._meter_data[topic] = val
+ 
+    def _mqtt_on_connect(self, client, userdata, rc, *args): 
+        print('Succesfully connected to MQTT broker')
+        client.subscribe(self._mqtt_topic + "/#")
+ 
+def exit_on_error(func, *args, **kwargs):
+    try:
+        return func(*args, **kwargs)
+    except:
+        try:
+            print ('exit_on_error: there was an exception. Printing stacktrace will be tried and then exit')
+            print_exc()
+        except:
+            pass
+        # sys.exit() is not used, since that throws an exception, which does not lead to a program
+        # halt when used in a dbus callback, see connection.py in the Python/Dbus libraries, line 230.
+        smartmeter_dbus.close()
+        os_exit(1)
+
+# Called on a one second timer
+def handle_timer_tick():
+    smartmeter_dbus.update()
+    return True  # keep timer running
+
+if __name__ == "__main__":  
+    # Have a mainloop, so we can send/receive asynchronous calls to and from dbus
+    print('SmartMeter to DBus started')
+    DBusGMainLoop(set_as_default=True)
+    mainloop = GLib.MainLoop()
+    GLib.timeout_add(500, lambda: exit_on_error(handle_timer_tick))
+    smartmeter_dbus = SmartMeterDBus('127.0.0.1', '', '', 'SmartMeter')
+    # Give some time to receive MQTT data
+    time.sleep(3)
+    mainloop.run()
